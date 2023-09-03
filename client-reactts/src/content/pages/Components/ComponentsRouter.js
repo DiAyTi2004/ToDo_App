e0065b1cfc6a5edc3261d6709030a968fef13b76
Nathan10252 <hoangvuong10252@gmail.com>
2023-09-03 23:59:16 +0700

    reorganize router files

--- client-reactts/src/content/pages/Components/ComponentsRouter.js
+++ client-reactts/src/content/pages/Components/ComponentsRouter.js
@@ -1,70 +1,30 @@
-import { Loader } from 'src/common/CommonFunctions'
-import { lazy } from 'react'
+import SidebarLayout from "src/layouts/SidebarLayout/SidebarLayoutIndex";
+import AccordionRouter from "./content/pages/Components/Accordions/AccordionsRouter";
+import AvatarsRouter from "./content/pages/Components/Avatars/AvatarsRouter";
+import BadgesRouter from "./content/pages/Components/Badges/BadgesRouter";
+import ButtonsRouter from "./content/pages/Components/Buttons/ButtonsRouter";
+import CardsRouter from "./content/pages/Components/Cards/CardsRouter";
+import FormsRouter from "./content/pages/Components/Forms/FormsRouter";
+import ModalsRouter from "./content/pages/Components/Modals/ModalsRouter";
+import TabsRouter from "./content/pages/Components/Tabs/TabsRouter";
+import TooltipsRouter from "./content/pages/Components/Tooltips/TooltipsRouter";
 
-export const AccordionsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Accordions/AccordionsIndex'))
-);
-export const AvatarsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Avatars/AvatarsIndex'))
-);
-export const BadgesPage = Loader(
-    lazy(() => import('src/content/pages/Components/Badges/BadgesIndex'))
-);
-export const ButtonsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Buttons/ButtonsIndex'))
-);
-export const CardsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Cards/CardsIndex'))
-);
-export const FormsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Forms/FormsIndex'))
-);
-export const ModalsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Modals/ModalsIndex'))
-);
-export const TabsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Tabs/TabsIndex'))
-);
-export const TooltipsPage = Loader(
-    lazy(() => import('src/content/pages/Components/Tooltips/TooltipsIndex'))
-);
 const ComponentsRouters = [
     {
-        path: "pages/accordions",
-        element: <AccordionsPage />
-    },
-    {
-        path: "pages/Avatars",
-        element: <AvatarsPage/>,
-    },
-    {
-        path: "pages/Badges",
-        element: <BadgesPage/>,
-    },
-    {
-        path: "pages/Buttons",
-        element: <ButtonsPage/>,
-    },
-    {
-        path: "pages/cards",
-        element: <CardsPage />
-    },
-    {
-        path: "pages/Forms",
-        element: <FormsPage />
-    },
-    {
-        path: "pages/Modals",
-        element: <ModalsPage />
-    },
-    {
-        path: "pages/Tabs",
-        element: <TabsPage />
-    },
-    {
-        path: "pages/Tooltips",
-        element: <tooltipsPage />
+        path: "components",
+        element: <SidebarLayout />,
+        children: [
+            ...AccordionRouter,
+            ...AvatarsRouter,
+            ...BadgesRouter,
+            ...ButtonsRouter,
+            ...CardsRouter,
+            ...FormsRouter,
+            ...ModalsRouter,
+            ...TabsRouter,
+            ...TooltipsRouter,
+        ]
     },
 ];
 
-export default ComponentsRouters
+export default ComponentsRouters;
