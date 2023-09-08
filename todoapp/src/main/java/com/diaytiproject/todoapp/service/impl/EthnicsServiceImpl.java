155c8da14ce06dea5ab19cc5ded6ba26c0793f71
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-09-08 20:31:18 +0700

    edit ethnics apis

--- todoapp/src/main/java/com/diaytiproject/todoapp/service/impl/EthnicsServiceImpl.java
+++ todoapp/src/main/java/com/diaytiproject/todoapp/service/impl/EthnicsServiceImpl.java
@@ -1,12 +1,10 @@
 package com.diaytiproject.todoapp.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
+import com.diaytiproject.todoapp.dto.EthnicsDto;
+import com.diaytiproject.todoapp.dto.SearchObject;
+import com.diaytiproject.todoapp.entity.Ethnics;
+import com.diaytiproject.todoapp.repository.EthnicsRepository;
+import com.diaytiproject.todoapp.service.EthnicsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -14,133 +12,138 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.diaytiproject.todoapp.dto.EthnicsDto;
-import com.diaytiproject.todoapp.dto.SearchObject;
-import com.diaytiproject.todoapp.entity.Ethnics;
-import com.diaytiproject.todoapp.repository.EthnicsRepository;
-import com.diaytiproject.todoapp.service.EthnicsService;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EthnicsServiceImpl implements EthnicsService {
-	@Autowired
-	private EthnicsRepository mainRepo;
+    @Autowired
+    private EthnicsRepository mainRepo;
 
-	@Autowired
-	private EntityManager manager;
+    @Autowired
+    private EntityManager manager;
 
-	@Override
-	public EthnicsDto getEthnicsById(UUID ethnicsId) {
-		if (ethnicsId == null)
-			return null;
-		Ethnics entity = mainRepo.findOneById(ethnicsId);
-		if (entity == null)
-			return null;
-		return new EthnicsDto(entity);
-	}
+    @Override
+    public EthnicsDto getEthnicsById(UUID ethnicsId) {
+        if (ethnicsId == null)
+            return null;
+        Ethnics entity = mainRepo.findOneById(ethnicsId);
+        if (entity == null)
+            return null;
+        return new EthnicsDto(entity);
+    }
 
-	@Override
-	public Page<EthnicsDto> pagingEthnics(SearchObject searchObj) {
-		if (searchObj == null)
-			return null;
-		int pageSize = searchObj.getPageSize();
-		int pageIndex = searchObj.getPageIndex();
-		pageIndex--;
-		if (pageIndex < 0)
-			pageIndex = 0;
+    @Override
+    public Page<EthnicsDto> pagingEthnics(SearchObject searchObj) {
+        if (searchObj == null)
+            return null;
+        int pageSize = searchObj.getPageSize();
+        int pageIndex = searchObj.getPageIndex();
+        pageIndex--;
+        if (pageIndex < 0)
+            pageIndex = 0;
 
-		String q = "select e from Ethnics e ";
-		String qCount = "select count(e.id) from Ethnics e ";
+        String q = "select e from Ethnics e where (1=1) ";
+        String qCount = "select count(e.id) from Ethnics e where (1=1) ";
 
-		String whereClause = "";
-		String keyword = "";
+        String whereClause = "";
+        String keyword = "";
 
-		if (searchObj.getKeyword() != null) {
-			keyword = searchObj.getKeyword().trim();
-			whereClause += "where e.code = :keyword or e.name = :keyword or e.description = :keyword";
-		}
+        if (searchObj.getKeyword() != null) {
+            keyword = searchObj.getKeyword().trim();
+            whereClause += "and e.code like :keyword or e.name like :keyword or e.description like :keyword";
+        }
 
-		q += whereClause;
-		qCount += whereClause;
+        q += whereClause;
+        qCount += whereClause;
 
-		Query sql = manager.createQuery(q);
-		Query sqlCount = manager.createQuery(qCount);
+        Query sql = manager.createQuery(q);
+        Query sqlCount = manager.createQuery(qCount);
 
-		Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
 
-		long totalRecords = (long) sqlCount.getSingleResult();
+        if (searchObj.getKeyword() != null) {
+            sql.setParameter("keyword", '%' + searchObj.getKeyword() + '%');
+            sqlCount.setParameter("keyword", '%' + searchObj.getKeyword() + '%');
+        }
 
-		List<EthnicsDto> data = new ArrayList<EthnicsDto>();
-		List<Ethnics> entities = sql.getResultList();
-		for (Ethnics entity : entities) {
-			data.add(new EthnicsDto(entity));
-		}
+        long totalRecords = (long) sqlCount.getSingleResult();
 
-		return new PageImpl<>(data, pageable, totalRecords);
-	}
+        List<EthnicsDto> data = new ArrayList<EthnicsDto>();
+        List<Ethnics> entities = sql.getResultList();
+        for (Ethnics entity : entities) {
+            data.add(new EthnicsDto(entity));
+        }
 
-	@Override
-	public List<EthnicsDto> getAllEthnics() {
-		List<Ethnics> entities = mainRepo.findAll();
-		List<EthnicsDto> data = new ArrayList<EthnicsDto>();
-		for (Ethnics entity : entities) {
-			data.add(new EthnicsDto(entity));
-		}
-		return data;
-	}
+        return new PageImpl<>(data, pageable, totalRecords);
+    }
 
-	@Override
-	public EthnicsDto createEthnics(EthnicsDto dto) {
-		if (dto == null)
-			return null;
-		Ethnics entity = new Ethnics();
-		entity.setCode(dto.getCode());
-		entity.setName(dto.getName());
-		entity.setDescription(dto.getDescription());
-		Ethnics response = mainRepo.save(entity);
-		return new EthnicsDto(response);
-	}
+    @Override
+    public List<EthnicsDto> getAllEthnics() {
+        List<Ethnics> entities = mainRepo.findAll();
+        List<EthnicsDto> data = new ArrayList<EthnicsDto>();
+        for (Ethnics entity : entities) {
+            data.add(new EthnicsDto(entity));
+        }
+        return data;
+    }
 
-	@Override
-	public EthnicsDto updateEthnics(EthnicsDto dto) {
-		if (dto == null)
-			return null;
-		if (dto.getId() == null)
-			return null;
-		Ethnics entity = mainRepo.findOneById(dto.getId());
-		entity.setCode(dto.getCode());
-		entity.setName(dto.getName());
-		entity.setDescription(dto.getDescription());
-		Ethnics response = mainRepo.save(entity);
-		return new EthnicsDto(response);
-	}
+    @Override
+    public EthnicsDto createEthnics(EthnicsDto dto) {
+        if (dto == null)
+            return null;
+        Ethnics entity = new Ethnics();
+        entity.setCode(dto.getCode());
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        Ethnics response = mainRepo.save(entity);
+        return new EthnicsDto(response);
+    }
 
-	@Override
-	public void deleteEthnics(UUID ethnicsId) {
-		if (ethnicsId == null)
-			return;
-		Ethnics entity = mainRepo.findOneById(ethnicsId);
-		if (entity == null)
-			return;
-		mainRepo.delete(entity);
-	}
+    @Override
+    public EthnicsDto updateEthnics(EthnicsDto dto) {
+        if (dto == null)
+            return null;
+        if (dto.getId() == null)
+            return null;
+        Ethnics entity = mainRepo.findOneById(dto.getId());
+        entity.setCode(dto.getCode());
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        Ethnics response = mainRepo.save(entity);
+        return new EthnicsDto(response);
+    }
 
-	@Override
-	public void deleteAllEthnics() {
-		mainRepo.deleteAll();
-	}
+    @Override
+    public void deleteEthnics(UUID ethnicsId) {
+        if (ethnicsId == null)
+            return;
+        Ethnics entity = mainRepo.findOneById(ethnicsId);
+        if (entity == null)
+            return;
+        mainRepo.delete(entity);
+    }
 
-	@Override
-	public void deleteListEthnics(List<UUID> ethnicsIds) {
-		if (ethnicsIds == null)
-			return;
-		for (UUID id : ethnicsIds) {
-			if (id == null)
-				continue;
-			Ethnics entity = mainRepo.findOneById(id);
-			if (entity == null)
-				continue;
-			mainRepo.delete(entity);
-		}
-	}
+    @Override
+    public void deleteAllEthnics() {
+        mainRepo.deleteAll();
+    }
+
+    @Override
+    public void deleteListEthnics(List<UUID> ethnicsIds) {
+        if (ethnicsIds == null)
+            return;
+        for (UUID id : ethnicsIds) {
+            if (id == null)
+                continue;
+            Ethnics entity = mainRepo.findOneById(id);
+            if (entity == null)
+                continue;
+            mainRepo.delete(entity);
+        }
+    }
 
 }
